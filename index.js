1a924fd62f72635efc7f6a5a24c1a41f5ceb86ec
Rory McLaughlin <rmclaughlin@nectarom.com>
2018-12-10 11:59:52 -0600

    Add comments, more configuration options, and function to check file size.

--- index.js
+++ index.js
@@ -2,28 +2,34 @@
 
 const argv = require('minimist')(process.argv.slice(2), {
     default: {
-        file: 'output.txt'
+        file: 'output.txt',
+        MiB: 1,
+        chunks: 4
     }
 });
 const fs = require('fs');
 const request = require('request-promise');
 
-if (!argv.url) throw Error('Download URL is required!');
+// Source URL must be specified through command line option.
+if (!argv.url) throw Error('Source URL is required!');
 
 const options = {
     method: 'GET',
     uri: argv.url
 }
 
-fs.writeFile(argv.file, '', err => {
-    if (err) console.log(err);
-    console.log("Successfully created new file.");
-});
-
+const checkFileSize = () => {
+    fs.stat(`./${argv.file}`, (err, stats) => {
+        if (err) console.log(err);
+        console.log(`Current File Size: ${stats.size}`);
+    });
+}
 const determineChunkRange = (step) => {
-    const chunk = 1048576;
-    const startOfRange = 1 + ((chunk * step));
-    const endOfRange = chunk + (chunk * step);
+    // 1 Mib = 1,048,576 B.
+    // Only 1 MiB chunks are downloaded.
+    const chunkSize = argv.MiB * 1048576;
+    const startOfRange = step === 0 ? 0 + ((chunkSize * step)) : 1 + ((chunkSize * step));
+    const endOfRange = chunkSize + (chunkSize * step);
 
     return {'Range': `bytes=${startOfRange}-${endOfRange}`}
 }
@@ -34,21 +40,30 @@ const getOptions = (step) => {
     return options;
 }
 
-const makeChunkRequest = (step) => {
+const makeRequest = (step) => {
     const options = getOptions(step);
+    console.log(step, options)
 
+    // make request to specified URL.
     request(options)
         .then(response => {
-            fs.appendFile(argv.file, response, err => {
-                if (err) console.log(err);
+            try {
+                fs.appendFileSync(argv.file, response);
                 console.log("Writing data to file.");
-            });
+            } catch (err) {
+                console.log(`Error appending to ${argv.file}`, err);
+            }
         })
         .catch(error => {
-            console.log(222, error)
+            console.log(`Error making request to ${argv.url}`, error)
         });
 }
 
-for (let i = 0; i < 4; i++) {
-    makeChunkRequest(i);
+fs.writeFileSync(argv.file, '');
+console.log("Successfully created new file.");
+
+// Make specified number of requests.
+for (let i = 0; i < argv.chunks; i++) {
+    checkFileSize();
+    makeRequest(i);
 }
